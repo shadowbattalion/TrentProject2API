855ba42d2eb585f355ac61a7d79e93939a43abba
shadowbattalion <hafizspaceumar@gmail.com>
2021-10-14 06:35:51 +0000

    add cookies and testing the add_case endpoint

--- index.js
+++ index.js
@@ -22,38 +22,45 @@ app.use(cookieParser());
 
 // set a cookie
 app.use(async function (req, res, next) {
+    //connect to db
+    await MongoUtil.connect(process.env.MONGO_URI, "ghost_sightings")
+    let db = MongoUtil.getDB();
     // check if client sent cookie
-    var cookie = req.cookies.cookieName;
-    if (cookie === undefined) {
-      // no: set a new cookie
-      var randomNumber=new ObjectId()
-      res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-      console.log('cookie created successfully');
+    let cookie = req.cookies.cookieName
+    //check if cookie in database
+    let cookie_in_db = await db.collection('witness').findOne({"_id":ObjectId(req.cookies.cookieName)})
 
+    if (cookie === undefined || cookie_in_db===null) {
+        // no: set a new cookie
+        let randomNumber=new ObjectId()
+        res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
+        console.log('cookie created successfully');
 
-      await MongoUtil.connect(process.env.MONGO_URI, "ghost_sightings")
-      let db = MongoUtil.getDB();
-
-    //   let update_in_cases = await db.collection('cases').updateOne({ 
-    //     "_id": ObjectId(case_id)
-    // }, {
-    //         $push: {
-    //             "comments": ObjectId(comment_id)
-    //         }
-    // })
+        let new_cookie = await db.collection('witness').insertOne({
+            "_id": randomNumber,
+            "registered":false,
+            "display_name":"",
+            "occupation":"",
+            "gender":"",
+            "age":"",
+            "company":"",
+            "investigator":false,
+            "email":"",
+            "cases":[]
+        })
       
-    db.close()
-     
+  
+        console.log(new_cookie)
 
     } else {
-      // yes, cookie was already present 
-      console.log('cookie exists', cookie);
+        // yes, cookie was already present 
+        console.log('cookie exists', cookie);
     } 
     next(); // <-- important!
-  });
+});
   
-  // let static middleware do its job
-  app.use(express.static(__dirname + '/public'));
+// let static middleware do its job
+app.use(express.static(__dirname + '/public'));
 
 async function main() {
 
@@ -75,7 +82,7 @@ async function main() {
         try {
             let db = MongoUtil.getDB();
     
-            console.log(req.cookies.cookieName)
+            
     
             let witnesses = await db.collection('witness').find().toArray()
                         
@@ -122,7 +129,7 @@ async function main() {
 
             let cases_id = req.params.id
             let cases_detail = await db.collection('cases').findOne({"_id":ObjectId(cases_id)})
-            let witness =  await db.collection('witness').find({"cases":ObjectId(cases_id)}).toArray()
+            let witness =  await db.collection('witness').findOne({"cases":ObjectId(cases_id)})
             
         
             encounter_details=[]
@@ -274,56 +281,173 @@ async function main() {
 
     })
 
-    //Add_case format:
-    // [
-    //      {
-    //        "occupation":"NSMan",
-    //        "gender":"male",
-    //        "age":"23",
-    //        "company":"",
-    //        "investigator":false,
-    //        "email":"test@this.com"
-    //      },
-    //      {
-    //     "case_title":"Ghost in Woodlands",
-    //     "generic_description":"This is a scary ghost adventure",
-    //     "type_of_activity":"Ghost-Hunting",
-    //     "rating":3,
-    //     "location":"Woodlands Drive 40",
-    //     "coordinates":[21.2, 32.4],
-    //     "date":"27-2034",
-    //     "entity_tags":["616509e7154896339f81b009","616509f8154896339f81b00a"]
-    //      },
-    //     [
-    //      {
-    //     "images":["http://test.com"],
-    //     "sightings_description":"The ghost can be seen here",
-    //     "equipment_used":["phone","camera"],
-    //     "contact_type":["visual"],
-    //     "number_of_entities":3,
-    //     "time_of_encounter":"midday"
-    //      },
-    //      {
-    //     "images":["http://test.com"],
-    //     "sightings_description":"The ghost can be seen here",
-    //     "equipment_used":["phone","camera"],
-    //     "contact_type":["visual"],
-    //     "number_of_entities":3,
-    //     "time_of_encounter":"midday"
-    //      }
-    //     ]
-    //  ]
+    
+
+    app.get("/check_registered", async(req,res)=>{
+
+        try {
+            let db = MongoUtil.getDB()
+            
+            let result=false
+            
+            
+
+            if(req.cookies.cookieName){
+
+                let registered = await db.collection('witness').findOne({"_id":ObjectId(req.cookies.cookieName)},{"projection":{"registered":1}})
+                result=registered.registered?true:false
+
+            }
+
+           
+        
+            
+            res.status(200)
+            res.send(result)
+
+
+
+        } catch (e) {
+            res.status(500)
+            res.send(e)         
+        }
+
+
+    })
+
+
+    // Add_case format:
+    // {
+    //      "witness":{
+    //                     "occupation":"NSMan",
+    //                     "display_name":"",
+    //                     "gender":"male",
+    //                     "age":"23",
+    //                     "company":"",
+    //                     "investigator":false,
+    //                     "email":"test@this.com"
+    //                 },
+    //     "case":     {
+    //                     "case_title":"Ghost in Woodlands",
+    //                     "generic_description":"This is a scary ghost adventure",
+    //                     "type_of_activity":"Ghost-Hunting",
+    //                     "rating":3,
+    //                     "location":"Woodlands Drive 40",
+    //                     "coordinates":[21.2, 32.4],
+    //                     "date":"27-2034",
+    //                     "entity_tags":["616509e7154896339f81b009","616509f8154896339f81b00a"]
+    //                 },
+    //     "encounters":[
+    //                     {
+    //                         "images":["http://test.com"],
+    //                         "sightings_description":"The ghost can be seen here",
+    //                         "equipment_used":["phone","camera"],
+    //                         "contact_type":["visual"],
+    //                         "number_of_entities":3,
+    //                         "time_of_encounter":"midday"
+    //                     },
+    //                     {
+    //                         "images":["http://test.com"],
+    //                         "sightings_description":"The ghost can be seen here",
+    //                         "equipment_used":["phone","camera"],
+    //                         "contact_type":["visual"],
+    //                         "number_of_entities":3,
+    //                         "time_of_encounter":"midday"
+    //                     }
+    //                 ]
+    // }
+
 
     app.post('/add_case', async (req, res) => {
 
         try {
             let db = MongoUtil.getDB()
             
-            
-            let case_id = new ObjectId()
             let user_input = req.body
 
+
+            //encounters
+            let encounters_id=[]
+
+            for(let encounter of user_input.encounters){
+
+                let encounter_id=new ObjectId()
+                encounters_id.push(encounter_id)
+                await db.collection('encounters').insertOne({
+                        "_id": encounter_id,
+                        "images":encounter.images,
+                        "sightings_description":encounter.sightings_description,
+                        "equipment_used":encounter.equipment_used,
+                        "contact_type":encounter.contact_type,
+                        "number_of_entities":encounter.number_of_entities,
+                        "time_of_encounter":encounter.time_of_encounter
+                })
+
+
+
+
+            }
+
+            //case
+
+            let case_id = new ObjectId()
+
+            await db.collection('cases').insertOne({
+                    "_id": cases_id,
+                    "case_title":user_input.cases.case_title,
+                    "generic_description":user_input.cases.generic_description,
+                    "type_of_activity":user_input.cases.type_of_activity,
+                    "rating":user_input.cases.rating,
+                    "location":user_input.cases.location,
+                    "coordinates":user_input.cases.coordinates,
+                    "date":user_input.cases.date,
+                    "entity_tags":user_input.cases.entity_tags, //remember to resolve this with ObjectId().
+                    "encounters":encounters_id,
+                    "comments":[]
+
+            })
+
+
+
+            // witness
             
+            if("witness" in user_input){
+
+                await db.collection('witness').updateOne({ 
+                    "_id": ObjectId(case_id)
+                }, {
+                        $set: {
+                            "registered":true,
+                            "display_name":user_input.witness.display_name,
+                            "occupation":user_input.witness.occupation,
+                            "gender":user_input.witness.gender,
+                            "age":user_input.witness.age,
+                            "company":user_input.witness.company,
+                            "investigator":user_input.witness.investigator,
+                            "email":user_input.witness.email,
+                            "cases":[case_id]
+                        }
+                })
+
+
+
+
+
+
+            }else{
+
+
+                await db.collection('witness').updateOne({ 
+                    "_id": ObjectId(case_id)
+                }, {
+                        $push: {
+                            "cases":[case_id]
+                        }
+                })
+
+
+
+            }
            
         
         
@@ -331,7 +455,7 @@ async function main() {
         
             
             res.status(200)
-            res.send(user_input)
+            res.send("New case added!")
 
 
 
