32c9faa967b666bd043ba3360e04e11d8874dcfc
shadowbattalion <hafizspaceumar@gmail.com>
2021-10-26 09:47:09 +0000

    removed some comments

--- index.js
+++ index.js
@@ -3,18 +3,11 @@ const cors = require('cors')
 require('dotenv').config()
 const MongoUtil = require('./MongoUtil.js')
 const ObjectId = require('mongodb').ObjectId;
-const cookieParser = require('cookie-parser');
+
 
 
 let app = express();
-// important for RESTFul API:
-
-// allow Express to process JSON payload
-// in POST, PUT and PATCH requests
 app.use(express.json());
-
-// enable CORS so that our React applications
-// hosted on a domain name can use it
 app.use(cors());
 
 
